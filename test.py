2713fa6afd56060afb4170494a93f177451ae6b9
kun <caozhentian@hanwang.com.cn>
2018-08-02 14:35:08 +0800

    改进

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+# -*- coding:utf-8 -*-
+from __future__ import unicode_literals
 import os
 import numpy as np
 import pygame
@@ -7,7 +9,213 @@ from hwcaptcha import makeDirDict,getHandwritingCaptcha
 from numpy import random
 from PIL import Image
 from generate_chars import CharsGenerator
+
+import json
 from yinhangcard import makePic
+
+from generate_word_training_data import byteify
+from testclass import Generate
+import sys
+
+
+
+print sys.getdefaultencoding()
+
+'''
+gener=Generate(len=10,size=3.5)
+fun="add"
+funct=getattr(gener,fun)
+gdcash=funct()
+gks=0
+'''
+
+ad={"fa":0,"g":1}
+del ad["g"]
+fs=0
+
+
+
+
+
+
+
+
+
+list_fn=[i for i in [1,2,3,4,5,6,7,8,9] if i>5]
+
+a = (x*x for x in range(10))
+#das= random.choice(a)
+
+'''
+path="/home/user/sample_generator/Synthetic_Data_Engine_For_Text_Recognition/ICDAR_2003/Word_Recognition_Train/word/word/Train/"
+pathlist=[]
+for file in os.listdir(path):
+    pathlist.append(os.path.join(path,file))
+ghks=0
+'''
+
+
+
+
+
+
+'''
+path=u"/home/user/0/标点1.txt"
+path1=u"/home/user/0/标点2.txt"
+with open(path) as ff:
+    with open(path1,"w") as ffw:
+        for line in ff:
+           try:
+             str_list=line.split()
+             line=str_list[1]
+             #line = line.encode("utf-8")
+           except Exception:
+               print(line)
+               continue
+           #line = line.encode("utf-8")
+
+           ffw.write(line+"\t\n")
+'''
+
+
+
+def multiple2(**args):
+    fs=len(args)
+    print(len(args))
+  #打印不定长参数
+    for key in args:
+      print key + ":" + bytes(args[key])
+#multiple2("a",1)
+multiple2(name='Amy', age=12, single=True)
+
+
+
+
+
+
+
+
+listpath=["ghsdkj","gj","hdf"]
+path="/"
+for i in listpath:
+    path=os.path.join(path,i)
+    gfj=0
+
+
+
+
+
+
+root=u"/home/user/sample_generator/"
+#root=os.path.split(os.path.realpath(__file__))[0]
+configPath=os.path.join(root,"配置文件")
+random_dir = os.path.join(root, "Synthetic_Data_Engine_For_Text_Recognition/SVT/svt/svt1/img")
+trainingchars_fn = os.path.join(root, "Synthetic_Data_Engine_For_Text_Recognition/ICDAR_2003/Word_Recognition_Train/word/word/Train/")
+for dirpath, dirnames, filenames in os.walk(configPath):
+    for file in filenames:
+        if file.endswith(".conf"):
+            path=os.path.join(dirpath,file)
+            with open(path) as ff:
+                jsonconfig = json.load(ff)
+                config = byteify(jsonconfig)
+            FontDir=os.path.join(dirpath,"font")
+
+            labelBackgdir = os.path.join(dirpath, "background")
+            output_dir=os.path.join(root, "result",config["label"].decode("utf-8" )+"_output_dir/")
+
+            #config["randomBackgdir"]=random_dir
+            config["trainingchars_fn"]=trainingchars_fn
+            strr=config["char_config"]["charDir"]
+            hfjds=type(strr)
+            gfds=type("hnd")
+            if isinstance(strr,str):
+                strr=strr.split("/")
+                find=False
+                charDir=root
+                for nu in strr:
+                    if not find and nu=="cortext":
+                        find=True
+                    if find:
+                        charDir = os.path.join(charDir, nu)
+                if find:
+                    config["char_config"]["charDir"]=charDir
+
+            config["char_config"]["FontDir"]=FontDir.decode("utf-8" )
+            config["labelBackgdir"] = labelBackgdir.decode("utf-8")
+            try:
+                del config["rotateAngle"]
+            except Exception:
+                pass
+            '''
+            config["output_config"]["keep_label"]=1
+            config["char_config"]["lineChars"] = config["lineChars"]
+            config["char_config"]["spaceVer"] = config["spaceVer"]
+            del config["lineChars"]
+            del config["spaceVer"]
+            
+            temp={}
+            temp["output_dir"]=output_dir.decode("utf-8")
+            temp["back_adjust"] = "None"
+            temp["output_height"] = [64 , 72]
+            temp["output_width"] = [240 , 270]
+            temp["ver_random"] = 1
+            temp["hor_random"] = 0
+            temp["keep_label"] = 1
+            temp["red"] = 1
+            config["output_config"] =temp
+            temp = {}
+            temp["isNoise"] = 0
+            temp["useOriBck"] = 1
+            temp["randomBackgdir"] = config["randomBackgdir"]
+            temp["rotateAngle"] = 10
+            config["noise_config"] = temp
+
+            temp = {}
+            temp["FontDir"] = config["FontDir"]
+            temp["FontSize"] = config["FontSize"]
+            temp["charDir"] = config["charDir"]
+            temp["char_spacing"] = config["char_spacing"]
+            temp["leftAdjust"] = config["leftAdjust"]
+            config["char_config"] = temp
+            
+            
+            del config["randomBackgdir"]
+            del config["FontDir"]
+            del config["FontSize"]
+            del config["charDir"]
+            del config["char_spacing"]
+            del config["leftAdjust"]
+            del config["red"]
+            del config["keep_label"]
+            del config["output_dir"]
+            
+            
+            adjust_value = {}
+            adjust_value[u"《"] = 2.0
+            adjust_value[u"|"] = 1.5
+            adjust_value[u"“"] = 2.5
+            adjust_value[u"〈"] = 2.0
+            adjust_value[u"￥"] = 1
+            adjust_value[u"‘"] = 2.5
+            adjust_value[u"·"] = 2.0
+            adjust_value[u"（"] = 1.7
+            adjust_value[u"１"] = 1.7
+            adjust_value[u"I"] = 1
+            adjust_value[u"1"] = 0.5
+            config["leftAdjust"] = adjust_value
+            '''
+            with open(path,'w') as ff:
+                ff.write(json.dumps(config, indent=1,ensure_ascii=False))
+
+
+
+
+
+
+print "end"
+
+
+
 
 #for i in create_amount_in_word():
  #   print(i)
